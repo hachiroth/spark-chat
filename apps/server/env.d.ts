4cf0c7cfcca55e32af480b1dc990d76e83fa4a65
Hachiroth <17876972635@163.com>
2025-07-25 00:09:39 +0800

    feat(server): support express type expansion

--- apps/server/env.d.ts
+++ apps/server/env.d.ts
@@ -1,7 +1,24 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    PORT: string
-    NODE_ENV: 'development' | 'production' | 'test'
-    SECRET: string
+import type { Session } from 'express-session'
+
+declare module 'express-session' {
+  interface Session {}
+}
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      PORT: string
+      NODE_ENV: 'development' | 'production' | 'test'
+      SECRET: string
+    }
+  }
+
+  namespace Express {
+    interface Request {
+      session: Session
+    }
+    interface Response {}
+    interface Locals {}
+    interface Application {}
   }
 }
